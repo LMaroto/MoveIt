dd666f9b14488131cadb7b56e49ffd03cdb20774
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-16 01:30:35 -0300

    Adiciona valor padrão para cookie inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import {GetServerSideProps} from 'next'
+import { GetServerSideProps } from 'next'
 
 import { CompletedChallenges } from '../components/CompletedChallenges/CompletedChallenges';
 import { Countdown } from '../components/Countdown/Countdown';
-import {ExperienceBar} from '../components/ExperienceBar/ExperienceBar';
+import { ExperienceBar } from '../components/ExperienceBar/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox/ChallengeBox';
 
@@ -13,41 +13,41 @@ import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 
 export default function Home(props) {
-  
+
   return (
-    <ChallengesProvider 
+    <ChallengesProvider
       level={props.level}
       currentExperience={props.currentExperience}
       challengesCompleted={props.challengesCompleted}
     >
-    <div className={styles.container}>
-    <Head>
-      <title>Inicio | move.it</title>
-    </Head>
-    
-    <ExperienceBar />
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
 
-    <CountdownProvider>
-    <section>
-      <div>
-        <Profile/>
-        <CompletedChallenges />
-        <Countdown />
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
       </div>
-    
-      <div>
-        <ChallengeBox />
-      </div>
-    </section>
-    </CountdownProvider>
-    </div>
     </ChallengesProvider>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  
-  const { level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  const { level, currentExperience = 0, challengesCompleted = 0 } = ctx.req.cookies;
 
   return {
     props: {
